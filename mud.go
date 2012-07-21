196dc23b3a7823d9cc1a7fb7d5beb89386a4fb13
Oliver Newland <onewland@gmail.com>
2012-07-20 19:47:05 -0700

    Added FlipFlop, an NPC that changes its name.
    FlipFlop's name will be the first persistent data.

--- mud.go
+++ mud.go
@@ -51,6 +51,49 @@ func (c *HeartbeatClock) UpdateTimeLoop() {
 	for { c.counter = <- c.tPing }
 }
 
+type FlipFlop struct {
+	mud.NPC
+	mud.Persister
+	room *mud.Room
+	stimuli chan mud.Stimulus
+	id int
+	lastText string
+}
+
+func (f FlipFlop) ID() int { return f.id }
+func (f FlipFlop) Name() string { return f.lastText }
+// Only respond to Talk stimulus to copy them
+func (f FlipFlop) DoesPerceive(s mud.Stimulus) bool {
+	_, ok := s.(mud.TalkerSayStimulus)
+	return ok
+}
+func (f FlipFlop) TextHandles() []string { return []string{} }
+func (f *FlipFlop) HandleStimulus(s mud.Stimulus) {
+	scast, ok := s.(mud.TalkerSayStimulus)
+	if !ok {
+		panic("Puritan should only receive TalkerSayStimulus")
+	} else {
+		args := strings.SplitN(scast.Text()," ",3)
+		fmt.Println("FF args:",args)
+		if(args[0] == "bling") {
+			switch(args[1]) {
+			case "set":
+				f.lastText = args[2]
+			}
+		}
+	}
+}
+func (f FlipFlop) StimuliChannel() chan mud.Stimulus {
+	return f.stimuli
+}
+func (f FlipFlop) Visible() bool { return true }
+func (f FlipFlop) Description() string { return f.Name() }
+func (f FlipFlop) Carryable() bool { return false }
+func (f FlipFlop) Save() (string, bool) {
+	finished := false
+	return "x",finished
+}
+
 type Puritan struct {
 	mud.Talker
 	mud.NPC
@@ -101,6 +144,14 @@ func (p Puritan) Visible() bool { return true }
 func (p Puritan) Description() string { return p.Name() }
 func (p Puritan) Carryable() bool { return false }
 
+func MakeFlipFlop() *FlipFlop {
+	ff := new(FlipFlop)
+	ff.id = 101
+	ff.lastText = "Unchanged."
+	ff.stimuli = make(chan mud.Stimulus, 5)
+	return ff
+}
+
 func MakePuritan() *Puritan {
 	puritan := new(Puritan)
 	puritan.id = 100
@@ -116,17 +167,20 @@ func MakeClock() *HeartbeatClock {
 
 func MakeStupidRoom(universe *mud.Universe) *mud.Room {
 	puritan := MakePuritan()
+	ff := MakeFlipFlop()
 	theBall := Ball{}
 	theClock := MakeClock()
 	universe.TimeListeners = []mud.TimeListener{theClock}
-	ballSlice := []mud.PhysicalObject{theBall, theClock, puritan}
+	ballSlice := []mud.PhysicalObject{theBall, theClock, puritan, ff}
 	empty := []mud.PhysicalObject{}
 
 	room := mud.NewBasicRoom(universe, 1, "You are in a bedroom.", ballSlice)
 	room.AddPerceiver(puritan)
+	room.AddPerceiver(ff)
 	room2 := mud.NewBasicRoom(universe, 2, "You are in a bathroom.", empty)
 	puritan.room = room
 	go mud.StimuliLoop(puritan)
+	go mud.StimuliLoop(ff)
 
 	mud.ConnectEastWest(room, room2)
 
